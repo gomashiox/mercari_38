685d52a68de531cbf3bb1f942ff4da6e6fb25fe8
googoogam <ken@kennoMacBook.local>
2018-11-23 12:19:01 +0900

    add Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'items#index'
+  root 'users#index'
   get '/users/show', to: 'users#show'
-
+  get   'users/:id'   =>  'users#show'
   devise_for :users
 
 
+  resources :items do
+    collection do
+      get 'get_category_ms' # /profiles/get_cities
+    end
+  end
 end
