a805bd6059d908a8c27567b111630ed85d279598
googoogam <ken@kennoMacBook.local>
2018-11-28 18:22:51 +0900

    Create js

--- app/assets/javascripts/preview-edit.js
+++ app/assets/javascripts/preview-edit.js
@@ -5,14 +5,16 @@ $(".modalOpen1").click(function(){
 
     var navClass = $(this).attr("class"),
         href = $(this).attr("href");
+    var src = $("#img1").attr('src');
+        $("#canvas").empty();
         $("#canvas").attr("src", $("#img1").attr('src'));
         $("#canvas").attr("title", $("#img1").attr('title'));
-console.log("test2");
         $(href).fadeIn();
     $(this).addClass("open");
-
     return false;
 });
+
+
 
 // モーダルウィンドウが閉じるときの処理
 $("#preview-modalClose").click(function(){
@@ -25,11 +27,11 @@ $("#preview-edit-cancell").click(function(){
     $(".modalOpen").removeClass("open");
     return false;
 });
-$("#preview-edit-finish").click(function(){
-    $(this).parents(".preview-modal").fadeOut();
-    $(".modalOpen").removeClass("open");
-    return false;
-});
+// $("#preview-edit-finish").click(function(){
+//     $(this).parents(".preview-modal").fadeOut();
+//     $(".modalOpen").removeClass("open");
+//     return false;
+// });
 });
 
 
